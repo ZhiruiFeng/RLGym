f37b775d13d7bdd2d06421938fcd4a590f0bf7fc
ZhiruiFeng <fihezro@gmail.com>
2017-12-03 17:27:57 -0800

    reset user2

--- lib/toyEnv/TicTacToe.py
+++ lib/toyEnv/TicTacToe.py
@@ -29,8 +29,6 @@ class BoardEnv:
         self.end = None
         self.winner = None
 
-
-
     # Give an unique hash value to every states
     # There are pow(3, rows * cols) states in total
     def getHash(self):
