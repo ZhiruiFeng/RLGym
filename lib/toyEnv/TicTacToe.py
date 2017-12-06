50180c069d402a78a3e262c07ea90083948646d9
ZhiruiFeng <fihezro@gmail.com>
2017-12-05 21:09:39 -0800

    add player and related reinforcement learning algorithm

--- lib/toyEnv/TicTacToe.py
+++ lib/toyEnv/TicTacToe.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import pickle
 
 ROWS_TRADITION = 3
 COLS_TRADITION = ROWS_TRADITION
@@ -152,17 +153,171 @@ class Judger:
                 self.currentPlayer == self.p1
             if show:
                 self.currentState.boardPrint()
-        [i, j, symbol] = self.currentPlayer.takeAction()
-        self.currentState = self.currentState.moveToNext(i, j, symbol)
-        hashValue = self.currentState.getHash()
-        self.currentState, isEnd = self.allStates[hashValue]
-        self.feedCurrentState()
-        if isEnd:
-            if self.feedback:
-                self.giveReward()
-            return self.currentState.winner 
+            [i, j, symbol] = self.currentPlayer.takeAction()
+            self.currentState = self.currentState.moveToNext(i, j, symbol)
+            hashValue = self.currentState.getHash()
+            self.currentState, isEnd = self.allStates[hashValue]
+            self.feedCurrentState()
+            if isEnd:
+                if self.feedback:
+                    self.giveReward()
+                return self.currentState.winner 
 
+# AI player
+class Player:
+    # @stepSize: step size to update estimations
+    # @exploreRate: possibility to explore
+    def __init__(self, stepSize = 0.1, exploreRate = 0.1):
+        self.allStates = allStates
+        self.estimations = dict()
+        self.stepSize = stepSize
+        self.exploreRate = exploreRate
+        self.states = []
 
+    def reset(self):
+        self.states = []
+
+    def setSymbol(self, symbol):
+        self.symbol = symbol
+        for i in self.allStates.keys():
+            (state, isEnd) = self.allStates[i]
+            if isEnd:
+                if state.winner == self.symbol:
+                    self.estimations[i] = 1.0
+                else:
+                    self.estimations[i] = 0
+            else:
+                self.estimations[i] = 0.5
+
+    def feedState(self, state):
+        self.states.append(state)
+
+    def feedReward(self, reward):
+        if len(self.states) == 0:
+            return
+        self.states = [state.getHash() for state in self.states]
+        target = reward
+        for latestState in reversed(self.states):
+            value = self.estimations[latestState] + self.stepSize * (target - self.estimations[latestState])
+            self.estimations[latestState] = value
+            target = value
+        self.states = []
+
+    def takeActions(self):
+        state = self.states[-1]
+        nextStaes = []
+        nextPositions = []
+        for i in range(BoardEnv.rows):
+            for j in range(BoardEnv.cols):
+                if state.data[i, j] == 0:
+                    nextPositions.append([i, j])
+                    nextStaes.append(state.nextStaes(i, j, self.symbol).getHash())
+        if np.random.binomial(1, self.exploreRate):
+            np.random.shuffle(nextPositions)
+            self.states = []
+            action = nextPositions[0]
+            action.append(self.symbol)
+            return action
+
+        value = []
+        for ha, pos in zip(nextStaes, nextPositions):
+            values.append((self.estimations[ha], pos))
+        np.random.shuffle(values)
+        values.sort(key=lambda x:x[0], reverse=True)
+        action = values[0][1]
+        action.append(self.symbol)
+        return action
+
+    def savePolicy(self):
+        fw = open('optimal_policy_' + str(self.symbol), 'wb')
+        pickle.dump(self.estimations, fw)
+        fw.close()
+
+    def loadPolicy(self):
+        fr = open('optimal_policy_' + str(self.symbol), 'rb')
+        self.estimations = pickle.load(fr)
+        fr.close()
+
+# human interface
+# input a number to put a chessman
+# | 1 | 2 | 3 |
+# | 4 | 5 | 6 |
+# | 7 | 8 | 9 |
+class HumanPlayer:
+    def __init__(self, stepSize = 0.1, exploreRate=0.1):
+        self.symbol = None
+        self.currentState = None
+        return
+    def reset(self):
+        return
+    def setSymbol(self, symbol):
+        self.symbol = symbol
+        return
+    def feedState(self, state):
+        self.currentState = state
+        return
+    def feedReward(self, reward):
+        return
+    def takeAction(self):
+        data = int(input("Input your position:"))
+        data -= 1
+        i = data // int(BoardEnv.cols)
+        j = data % BoardEnv.cols
+        if self.currentState.data[i, j] != 0:
+            return self.takeAction()
+        return (i, j, self.symbol)
+
+def train(epochs = 20000):
+    player1 = Player()
+    player2 = Player()
+    judger = Judger(player1, player2)
+    player1Win = 0.0
+    player2Win = 0.0
+    for i in range(0, epochs):
+        print("Epoch", i)
+        winner = judger.play()
+        if winner == 1:
+            player1Win += 1
+        if winner == -1:
+            player2Win += 1
+        judger.reset()
+    print(player1Win / epochs)
+    print(player2Win / epochs)
+    player1.savePolicy()
+    player2.savePolicy()
+
+def compete(turns=500):
+    player1 = Player(exploreRate=0)
+    player2 = Player(exploreRate=0)
+    judger = Judger(player1, player2, False)
+    player1.loadPolicy()
+    player2.loadPolicy()
+    player1Win = 0.0
+    player2Win = 0.0
+    for i in range(0, turns):
+        print("Epoch", i)
+        winner = judger.play()
+        if winner == 1:
+            player1Win += 1
+        if winner == -1:
+            player2Win += 1
+        judger.reset()
+    print(player1Win / turns)
+    print(player2Win / turns)
+
+def play():
+    while True:
+        player1 = Player(exploreRate=0)
+        player2 = HumanPlayer()
+        judger = Judger(player1, player2, False)
+        player1.loadPolicy()
+        winner = judger.play(True)
+        if winner == player2.symbol:
+            print("Win!")
+        elif winner == player1.symbol:
+            print("Lose!")
+        else:
+            print("Tie!")
 
 theEnv = BoardEnv()
 theEnv.moveToNext(1,1,-1)
